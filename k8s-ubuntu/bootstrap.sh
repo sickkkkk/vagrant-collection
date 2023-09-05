ab3d8c3e94d0d536b602fb0d5473e4341f8bc507
sickkkkk <stfckp@@gmail.com>
2023-09-05 10:17:44 +0400

    [no ci] update k8s-ubuntu multinode boostrap

--- k8s-ubuntu/bootstrap.sh
+++ k8s-ubuntu/bootstrap.sh
@@ -5,7 +5,6 @@
 ## This script is tested only in the generic/ubuntu2204 Vagrant box
 ## If you use a different version of Ubuntu or a different Ubuntu Vagrant box test this again
 #
-
 set -x
 
 echo "[TASK 1] Disable and turn off SWAP"
@@ -32,39 +31,38 @@ EOF
 sysctl --system 
 
 echo "[TASK 5] Install containerd runtime"
-sudo apt update
-sudo apt install -y ca-certificates curl gnupg lsb-release
-mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update 
-sudo apt install -y containerd.io 
-sudo containerd config default > /etc/containerd/config.toml
-sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
-sudo systemctl restart containerd
-sudo systemctl enable containerd 
+apt update -y
+apt install containerd -y
+systemctl enable containerd 
+systemctl start containerd 
+mkdir -p /etc/containerd/
+containerd config default | sudo tee /etc/containerd/config.toml
+sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+sed -i 's/sandbox_image \= "registry.k8s.io\/pause:3.8"/sandbox_image \= "registry.k8s.io\/pause:3.9"/g' /etc/containerd/config.toml
+systemctl restart containerd
 
-echo "[TASK 6] Add apt repo for kubernetes"
-sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - 
-sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main" 
+echo "[TASK 6] Install kubeadm from deb"
+# apt-transport-https may be a dummy package; if so, you can skip that package
+apt-get install -y apt-transport-https ca-certificates curl
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /" | tee /etc/apt/sources.list.d/kubernetes.list
 
-echo "[TASK 7] Install Kubernetes components (kubeadm, kubelet and kubectl)"
-sudo apt install -y kubeadm=1.24.0-00 kubelet=1.24.0-00 kubectl=1.24.0-00 
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
 
-echo "[TASK 8] Enable ssh password authentication"
+echo "[TASK 7] Enable ssh password authentication"
 sed -i 's/^PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config
 echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
-sudo systemctl reload sshd
+systemctl reload sshd
 
-echo "[TASK 9] Set root password"
-sudo echo -e "kubeadmin\nkubeadmin" | passwd root 
+echo "[TASK 8] Set root password"
+echo -e "kubeadmin\nkubeadmin" | passwd root 
 echo "export TERM=xterm" >> /etc/bash.bashrc
 
-echo "[TASK 10] Update /etc/hosts file"
-sudo cat >>/etc/hosts<<EOF
+echo "[TASK 9] Update /etc/hosts file"
+cat >>/etc/hosts<<EOF
 172.16.55.50   kmaster.int.ohmylab.io     kmaster
-172.16.55.61   kworker1.int.ohmylab.io    kworker1
-172.16.55.62   kworker2.int.ohmylab.io    kworker2
+172.16.55.61   kworker01.int.ohmylab.io    kworker01
+172.16.55.62   kworker02.int.ohmylab.io    kworker02
 EOF
--- k8s-ubuntu/bootstrap_kmaster.sh
+++ k8s-ubuntu/bootstrap_kmaster.sh
@@ -4,10 +4,17 @@ echo "[TASK 1] Pull required containers"
 sudo kubeadm config images pull 
 
 echo "[TASK 2] Initialize Kubernetes Cluster"
-sudo kubeadm init --apiserver-advertise-address=172.16.55.50 --pod-network-cidr=192.168.0.0/16 >> /root/kubeinit.log
+sudo kubeadm init --apiserver-advertise-address=172.16.55.50 --pod-network-cidr=192.168.0.0/16
 
 echo "[TASK 3] Deploy Calico network"
-sudo kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://docs.projectcalico.org/v3.18/manifests/calico.yaml 
+kubectl --kubeconfig=/etc/kubernetes/admin.conf apply -f https://docs.projectcalico.org/manifests/calico.yaml
 
 echo "[TASK 4] Generate and save cluster join command to /joincluster.sh"
 sudo kubeadm token create --print-join-command > /joincluster.sh
+
+echo "[TASK 5] Install qol tips for kubeadm"
+kubectl completion bash | sudo tee /etc/bash_completion.d/kubectl > /dev/null
+echo 'alias k=kubectl' >> ~/.bashrc 
+echo 'complete -o default -F __start_kubectl k' >> ~/.bashrc
+echo 'KUBECONFIG=/etc/kubernetes/admin.conf' >> ~/.bashrc
+source ~/.bashrc
