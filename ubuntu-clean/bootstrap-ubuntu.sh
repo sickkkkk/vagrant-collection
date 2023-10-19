9d00d34743da794948b11799e0f5a0f6b2f0e64d
sickkkkk <stfckp@gmail.com>
2023-10-19 13:26:21 +0400

    [no ci] update ubuntu-clean

--- ubuntu-clean/bootstrap-ubuntu.sh
+++ ubuntu-clean/bootstrap-ubuntu.sh
@@ -1,2 +1,27 @@
 echo "[TASK 1] Update DEPs"
 apt-get update -y && apt-get install
+
+# Add Docker's official GPG key:
+sudo apt-get update -y
+sudo apt-get install ca-certificates curl gnupg -y
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+sudo docker run hello-world
+
+sudo groupadd docker
+
+sudo usermod -aG docker $USER
+sudo usermod -aG docker vagrant
+
+sudo apt-get install nginx -y
