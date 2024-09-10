787dbb42d772c8c76417684afe561897cbe010a1
Sergo Valiuzhenich <sergo.valiuzhenich@nexus.ge>
2024-09-10 18:44:51 +0400

    [no ci] add centos9-arm starter boxes

---
+++ ubuntu-arm-k8s-sbx/bootstrap/bootstrap_master.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
+apt update -y
+apt install policycoreutils selinux-utils selinux-basics -y
+echo "Disable and turn off SWAP"
+sed -i '/swap/d' /etc/fstab
+swapoff -a
+
+echo "Disable ufw"
+systemctl disable ufw && systemctl stop ufw
+
+echo "Update hosts file"
+cat >>/etc/hosts<<EOF
+172.18.50.50    ansible-master.cluster.local    ansible-master
+172.18.50.55    kmaster.cluster.local    kmaster
+172.18.50.61    kworker01.cluster.local  kworker01
+172.18.50.62    kworker02.cluster.local  kworker02
+EOF
+
+mkdir -p /home/vagrant/.ssh
+echo $(cat /home/vagrant/vagrant-key/vagrant-bootstrap-key.pub) >> /home/vagrant/.ssh/authorized_keys
+chmod 600 /home/vagrant/.ssh/authorized_keys
+chown -R vagrant:vagrant /home/vagrant/.ssh
+
+echo 'eval $(ssh-agent -s)' >> /home/vagrant/.bashrc
+echo 'ssh-add /home/vagrant/vagrant-key/vagrant-bootstrap-key' >> /home/vagrant/.bashrc
