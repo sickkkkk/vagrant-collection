8a8531a13721acb9f146fd5a24188df9c097b93e
Sergo Valiuzhenich <sergo.valiuzhenich@nexus.ge>
2024-10-03 17:11:11 +0400

    add haprixy role

--- ubuntu-arm-k8s-sbx/bootstrap/bootstrap_node.sh
+++ ubuntu-arm-k8s-sbx/bootstrap/bootstrap_node.sh
@@ -5,4 +5,5 @@ cat >>/etc/hosts<<EOF
 172.18.50.71    kmaster1.cluster.local    kmaster1
 172.18.50.72    kmaster2.cluster.local    kmaster2
 172.18.50.61    kworker.cluster.local  kworker
+172.18.50.100   haproxy.cluster.local  haproxy
 EOF
--- ubuntu-arm-k8s-sbx/scripts/local/copy-id-control-node.sh
+++ ubuntu-arm-k8s-sbx/scripts/local/copy-id-control-node.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
-KEY_FILE_PATH="../../vagrant-key/vagrant-bootstrap-key.pub" #key path to local file
-for host in kmaster1.cluster.local kmaster2.cluster.local kworker.cluster.local; do
-    echo "Adding identity to $host"
-    ssh-copy-id -i $KEY_FILE_PATH vagrant@$host
+INVENTORY_FILE="../../ansible/inventory.yml"
+KEY_FILE_PATH="../../vagrant-key/vagrant-bootstrap-key.pub"
+REMOTE_USER="vagrant"
+KNOWN_HOSTS_FILE=~/.ssh/known_hosts
+if [[ ! -f "$INVENTORY_FILE" || ! -f "$KEY_FILE_PATH" ]]
+then
+    echo "Nor keyfile nor inventory found. Aborting"
+    exit 1
+fi
+if ! command -v jq &> /dev/null
+then
+    echo "jq is required but not installed. Please install jq and retry."
+    exit 1
+fi
+HOSTS=$(ansible-inventory -i "$INVENTORY_FILE" --list | jq -r '._meta.hostvars | to_entries[] | .value.ansible_host')
+echo "$HOSTS" | while IFS= read -r host; do
+    echo "Adding identity $REMOTE_USER@$host"
+    ssh-copy-id -i "$KEY_FILE_PATH" "$REMOTE_USER@$host"
+    echo "Adding host key for $host to known_hosts"
+    ssh-keyscan -t ed25519 "$host" >> "$KNOWN_HOSTS_FILE" 2>/dev/null
 done
